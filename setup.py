161b7d91e308e2c1ec4c4add51c4beb77ea56451
Malik Arslan Bashir <malikarslaninfo575@gmail.com>
2025-01-02 00:42:50 +0500

    Reorganize project structure

--- setup.py
+++ setup.py
@@ -1,30 +1,30 @@
-import setuptools
+from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
-
-
+# Metadata
 __version__ = "0.0.0"
-
 REPO_NAME = "Text-Summarizer"
 AUTHOR_USER_NAME = "MalikArslanBashir"
 SRC_REPO = "textsummarizer"
 AUTHOR_EMAIL = "malikarslaninfo575@gmail.com"
 
+# Read long description from README.md
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
 
-
-setuptools.setup(
+# Setup configuration
+setup(
     name=SRC_REPO,
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
     description="A small python package for NLP app",
     long_description=long_description,
-    long_description_content="text/markdown",
+    long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src")
+    packages=find_packages(where="src"),  # Automatically discovers packages in 'src'
+    install_requires=[],  # Add dependencies if needed
 )
